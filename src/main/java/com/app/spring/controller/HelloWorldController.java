9404623232bbd6a82c32289839bfe5fb283ad378
cesaraugusto0 <augustocsar22@gmail.com>
2025-02-22 08:42:03 -0300

    Adicionando mudança

--- src/main/java/com/app/spring/controller/HelloWorldController.java
+++ src/main/java/com/app/spring/controller/HelloWorldController.java
@@ -10,6 +10,6 @@ public class HelloWorldController {
 
     @GetMapping
     public String helloWorld() {
-        return "Hello World!";
+        return "Hello World! Adicionando mudança!";
     }
 }
