c0456759bc5a0463de56bb4e14e0d0573044924d
cesaraugusto0 <augustocsar22@gmail.com>
2025-02-22 08:51:05 -0300

    Add controller home

---
+++ src/main/java/com/app/spring/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.app.spring.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping
+    public String home() {
+        return "<h1>Página Home</h1>";
+    }
+}
